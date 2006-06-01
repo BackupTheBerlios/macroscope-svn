9fd91f177afef257e8fd4ab6633cffe1688e2815
white_raven <white_raven@5e8ca29b-5b08-0410-b372-a15a77fb793d>
2006-06-01 11:28:16 +0000

    stable FiberInterlockedMutex implementation

---
+++ branches/stable/test/msftpd/nv.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+versions="msftp"
+
+version_msftp="."
+
+wd=`pwd`;
+
+for i in $versions; do
+  eval version=\${i}
+  eval version_header=\$version_${i}
+  cd $version_header
+  if test ".$1" = ".s"; then
+    shtool version -l c -p ${version}_ -n $version -s $2 version.h
+  elif test ".$1" = ".v"; then
+    shtool version -l c -p ${version}_ -n $version -i v version.h
+  elif test ".$1" = ".r"; then
+    shtool version -l c -p ${version}_ -n $version -i r version.h
+  elif test ".$1" = ".l"; then
+    shtool version -l c -p ${version}_ -n $version -i l version.h
+  else
+    shtool version -l c -p ${version}_ -n $version -d long version.h
+  fi
+  cd $wd
+done
