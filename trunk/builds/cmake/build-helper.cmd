e6131247c543efa245953034fef0ca998e73402e
white_raven <white_raven@5e8ca29b-5b08-0410-b372-a15a77fb793d>
2006-07-31 08:12:12 +0000

    Migrate to CMake build system.

--- trunk/builds/cmake/build-helper.cmd
+++ trunk/builds/cmake/build-helper.cmd
@@ -1,74 +1,22 @@
 @echo off
 
-echo Build configuration Debug %x64%
-cmake -DCMAKE_BUILD_TYPE=Debug %x64%
-if errorlevel 0 goto buildDebug
+echo Build configuration %conf% %x64%
+cmake -DCMAKE_BUILD_TYPE=%conf% %x64%
+if errorlevel 0 goto build
 echo cmake execution failed
 exit 1
-:buildDebug
-%devenv% macroscope.sln /%cmd% Debug /Project ALL_BUILD.vcproj /Log
-if errorlevel 0 goto makeDebugPackage
-echo build debug configuration failed
+
+:build
+%devenv% macroscope.sln /%cmd% %conf% /Project ALL_BUILD.vcproj /Log
+if errorlevel 0 goto makePackage
+echo build %conf% configuration failed
 exit 1
-:makeDebugPackage
-echo Make package Debug
-%devenv% macroscope.sln /%cmd% Debug /Project PACKAGE.vcproj /Log
-if errorlevel 0 goto makeRelease
+
+:makePackage
+echo Make package %conf%
+%devenv% macroscope.sln /%cmd% %conf% /Project PACKAGE.vcproj /Log
+if errorlevel 0 goto exit0
 echo make debug package failed
 exit 1
 
-:makeRelease
-echo Build configuration Release %x64%
-cmake -DCMAKE_BUILD_TYPE=Release %x64%
-if errorlevel 0 goto buildRelease
-echo cmake execution failed
-exit 1
-:buildRelease
-%devenv% macroscope.sln /%cmd% Release /Project ALL_BUILD.vcproj /Log
-if errorlevel 0 goto makeReleasePackage
-echo build release configuration failed
-exit 1
-:makeReleasePackage
-echo Make package Release
-%devenv% macroscope.sln /%cmd% Release /Project PACKAGE.vcproj /Log
-if errorlevel 0 goto makeRelWithDebInfo
-echo make release package failed
-exit 1
-
-:makeRelWithDebInfo
-echo Build configuration RelWithDebInfo %x64%
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo %x64%
-if errorlevel 0 goto buildRelWithDebInfo
-echo cmake execution failed
-exit 1
-:buildRelWithDebInfo
-%devenv% macroscope.sln /%cmd% RelWithDebInfo /Project ALL_BUILD.vcproj /Log
-if errorlevel 0 goto makeRelWithDebInfoPackage
-echo build RelWithDebInfo configuration failed
-exit 1
-:makeRelWithDebInfoPackage
-echo Make package RelWithDebInfo
-%devenv% macroscope.sln /%cmd% RelWithDebInfo /Project PACKAGE.vcproj /Log
-if errorlevel 0 goto makeMinSizeRel
-echo make RelWithDebInfoPackage package failed
-exit 1
-
-:makeMinSizeRel
-echo Build configuration MinSizeRel %x64%
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel %x64%
-if errorlevel 0 goto buildMinSizeRel
-echo cmake execution failed
-exit 1
-:buildMinSizeRel
-%devenv% macroscope.sln /%cmd% MinSizeRel /Project ALL_BUILD.vcproj /Log
-if errorlevel 0 goto makeMinSizeRelPackage
-echo build MinSizeRel configuration failed
-exit 1
-:makeMinSizeRelPackage
-echo Make package MinSizeRel
-%devenv% macroscope.sln /%cmd% MinSizeRel /Project PACKAGE.vcproj /Log
-if errorlevel 0 goto success
-echo make MinSizeRel package failed
-exit 1
-
-:success
+:exit0
--- trunk/builds/cmake/build.cmd
+++ trunk/builds/cmake/build.cmd
@@ -1,9 +1,20 @@
 @echo off
 
 set devenv="C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.exe"
-set cmd=Rebuild
+set cmd=Build
 
+set conf=Debug
 call build-helper.cmd
+
+set conf=Release
+call build-helper.cmd
+
+set conf=MinSizeRel
+call build-helper.cmd
+
+set conf=RelWithDebInfo
+call build-helper.cmd
+
 rem set x64=-G "Visual Studio 8 2005 Win64"
 rem set cmd=Rebuild
 rem del CMakeCache.txt
