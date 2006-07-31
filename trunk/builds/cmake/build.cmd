9f41f8d8732be146b603705b79d2627e396696a6
white_raven <white_raven@5e8ca29b-5b08-0410-b372-a15a77fb793d>
2006-07-31 10:37:00 +0000

    Migrate to CMake build system finished. CMake with NSIS installer support added. Binaries versions added.

--- trunk/builds/cmake/build.cmd
+++ trunk/builds/cmake/build.cmd
@@ -5,17 +5,22 @@ set cmd=Build
 
 set conf=Debug
 call build-helper.cmd
-
 set conf=Release
 call build-helper.cmd
-
 set conf=MinSizeRel
 call build-helper.cmd
-
 set conf=RelWithDebInfo
 call build-helper.cmd
 
-rem set x64=-G "Visual Studio 8 2005 Win64"
-rem set cmd=Rebuild
-rem del CMakeCache.txt
-rem call build-helper.cmd
+set x64=-G "Visual Studio 8 2005 Win64"
+set cmd=Rebuild
+del CMakeCache.txt
+
+set conf=Debug
+call build-helper.cmd
+set conf=Release
+call build-helper.cmd
+set conf=MinSizeRel
+call build-helper.cmd
+set conf=RelWithDebInfo
+call build-helper.cmd
