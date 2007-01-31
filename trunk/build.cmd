42218984e1c41f538cbda3eea9315983b4fe7734
white_raven <white_raven@5e8ca29b-5b08-0410-b372-a15a77fb793d>
2007-01-31 12:43:25 +0000

    fix build under Intel C++ Compiler

--- trunk/build.cmd
+++ trunk/build.cmd
@@ -7,19 +7,18 @@ if /i %1 == x64       goto amd64
 if /i %1 == ia64      goto ia64
 
 :x86
-call "%INTEL_LICENSE_FILE%\..\..\..\Intel\Compiler\C++\9.1\IA32\Bin\iclvars.bat"
+call "%INTEL_LICENSE_FILE%\..\..\..\Intel\Compiler\C++\9.1\IA32\Bin\iclvars.bat" 1>%TEMP%\stdout.log 2>%TEMP%\stderr.log
 goto exit
 :amd64
-call "%INTEL_LICENSE_FILE%\..\..\..\Intel\Compiler\C++\9.1\EM64T\Bin\iclvars.bat"
+call "%INTEL_LICENSE_FILE%\..\..\..\Intel\Compiler\C++\9.1\EM64T\Bin\iclvars.bat" 1>%TEMP%\stdout.log 2>%TEMP%\stderr.log
 goto exit
 :ia64
-call "%INTEL_LICENSE_FILE%\..\..\..\Intel\Compiler\C++\9.1\Itanium\Bin\iclvars.bat"
+call "%INTEL_LICENSE_FILE%\..\..\..\Intel\Compiler\C++\9.1\Itanium\Bin\iclvars.bat" 1>%TEMP%\stdout.log 2>%TEMP%\stderr.log
 goto exit
 
 :exit
 
-call "%VS80COMNTOOLS%..\..\VC\vcvarsall.bat"
+call "%VS80COMNTOOLS%..\..\VC\vcvarsall.bat" 1>%TEMP%\stdout.log 2>%TEMP%\stderr.log
 
-cmake -DPRIVATE_RELEASE=1 -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" . &&
-rem del /s /f /q *.obj *.exe *.dll 1>null 2>null
-nmake && make package
+del /s /f /q *.obj *.exe *.dll 1>%TEMP%\stdout.log 2>%TEMP%\stderr.log
+cmake -DPRIVATE_RELEASE=1 -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" . && nmake && make package
