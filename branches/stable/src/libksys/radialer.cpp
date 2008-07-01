bed528ffc0f6de1c69bc7544a2a782ef2a30793f
white_raven <white_raven@5e8ca29b-5b08-0410-b372-a15a77fb793d>
2008-07-01 12:42:12 +0000

    macroscope freebsd port release

---
+++ branches/stable/src/libksys/radialer.cpp
@@ -0,0 +1,82 @@
+/*-
+ * Copyright 2006 Guram Dukashvili
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+//---------------------------------------------------------------------------
+#include <adicpp/ksys.h>
+//------------------------------------------------------------------------------
+namespace ksys {
+//------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------------------
+RemoteAccessDialer::~RemoteAccessDialer()
+{
+  close();
+}
+//------------------------------------------------------------------------------
+RemoteAccessDialer::RemoteAccessDialer()
+{
+#if defined(__WIN32__) || defined(__WIN64__)
+  hRasConn_ = NULL;
+#else
+  Exception::throwSP(ENOSYS,__PRETTY_FUNCTION__);
+#endif
+}
+//------------------------------------------------------------------------------
+RemoteAccessDialer & RemoteAccessDialer::open()
+{
+  if( isWin9x() ){
+    DWORD r = RasDialA(
+      &rasDialExtensions_,
+      phonebook_.getANSIString(),
+      &rasDialParamsA_,
+      NULL,
+      NULL,
+      &hRasConn_
+    );
+  }
+  else {
+    DWORD r = RasDialW(
+      &rasDialExtensions_,
+      phonebook_.getUNICODEString(),
+      &rasDialParamsW_,
+      NULL,
+      NULL,
+      &hRasConn_
+    );
+  }
+  return *this;
+}
+//------------------------------------------------------------------------------
+RemoteAccessDialer & RemoteAccessDialer::close()
+{
+  if( hRasConn_ == NULL ){
+    RasHangUp(hRasConn_);
+    hRasConn_ = NULL;
+  }
+  return *this;
+}
+//------------------------------------------------------------------------------
+} // namespace ksys
+//------------------------------------------------------------------------------
