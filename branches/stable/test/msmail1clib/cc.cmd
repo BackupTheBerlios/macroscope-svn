f160046d5920a7d10d178a27c710fbfcf9f83cfa
white_raven <white_raven@5e8ca29b-5b08-0410-b372-a15a77fb793d>
2008-07-28 07:59:12 +0000

    prepare freebsd port

--- branches/stable/test/msmail1clib/cc.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call "%VS80COMNTOOLS%..\..\VC\vcvarsall.bat"
-
-midl /D "_CRT_SECURE_NO_DEPRECATE" /D "HAVE_CONFIG_H" /D "CMAKE_BUILD" /D "CMAKE_SYSTEM_NAME="Windows"" /D "CMAKE_BUILD_TYPE=2" /D "_WIN32_WINNT=0x0502" /D "DISABLE_FIREBIRD_INTERFACE" /D "DISABLE_MYSQL_INTERFACE" /D "_USRDLL" /D "_MERGE_PROXYSTUB" /nologo /char signed /env win32 /Oicf  /tlb "msmail1clib.tlb" /out "msmail1clib.dir\Release" /h "msmail1clib.h" /iid "msmail1clib_i.c" /proxy "msmail1clib_p.c" .\msmail1clib.idl
